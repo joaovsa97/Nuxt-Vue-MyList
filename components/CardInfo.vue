e4d97949a991d467986a8263516615c94f523555
joaovsa97 <joaos.azevedo10@gmail.com>
2022-12-20 14:44:29 -0300

    Adicionado ícones e reestrutura do Card

--- components/CardInfo.vue
+++ components/CardInfo.vue
@@ -3,11 +3,15 @@
     <div class="text-center d-flex align-center justify-center mt-2">
       <h3>{{ item.nome }}</h3>
     </div>
-    <div>
+    <div class="imagem">
       <img :src="item.imagem" alt="" />
     </div>
     <div class="text-center">
       <h4>{{ item.descricao }}</h4>
+    </div>
+    <div class="pa-1">
+      <button><img src="../assets/visibility_black_24dp.svg" alt=""></button>
+      <button><img src="../assets/delete_black_24dp.svg" alt=""></button>
     </div>
   </div>
 </template>
@@ -30,7 +34,7 @@ export default {
   color: black;
   background-color: rgb(198, 157, 228);
 }
-#card img {
+.imagem img {
   width: 100%;
   height: 280px;
 }
